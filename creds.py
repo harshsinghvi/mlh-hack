cde2ca9e520597e56238b866544ae9dfbc15dbcc
Harsh Singhvi <root@harshsinghvi.com>
2020-12-20 13:10:42 +0530

    delete code due to unmet deadlines

--- creds.py
+++
@@ -1,7 +0,0 @@
-import os
-
-MONGO_DB_URI='mongodb+srv://admin:313001@cluster0.6jr3w.mongodb.net/hackathon?retryWrites=true&w=majority'
-
-# make collections named questions, answers , responses
-
-os.environ['MONGO_DB_URI'] = MONGO_DB_URI
